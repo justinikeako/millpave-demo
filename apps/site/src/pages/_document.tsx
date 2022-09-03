42a2584e5dd549d9bddd7f20dcc3c5956d2fefe6
Justin Ikeako <justinikeako@gmail.com>
2022-09-03 04:13:48 -0500

    Optimize fonts on site

---
+++ apps/site/src/pages/_document.tsx
@@ -0,0 +1,12 @@
+import { Html, Main, NextScript } from 'next/document';
+
+export default function Document() {
+	return (
+		<Html lang="en-US">
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+}

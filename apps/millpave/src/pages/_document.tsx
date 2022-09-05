1aa530cbccfc364b8bbf733c23e02df4551c0f27
Justin Ikeako <justinikeako@gmail.com>
2022-09-05 17:02:55 -0500

    Fix eslint issues

--- apps/millpave/src/pages/_document.tsx
+++ apps/millpave/src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
 			<Head>
 				<link
 					rel="stylesheet"
-					href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=block"
+					href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=optional"
 				/>
 			</Head>
 			<body>

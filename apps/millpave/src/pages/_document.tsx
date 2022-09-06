14fb0da72b4f2a8fa57d1e9effa8d01c09cda895
Justin Ikeako <justinikeako@gmail.com>
2022-09-06 04:01:08 -0500

    Fix Material Symbols display bug

--- apps/millpave/src/pages/_document.tsx
+++ apps/millpave/src/pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/google-font-display */
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
@@ -6,7 +7,7 @@ export default function Document() {
 			<Head>
 				<link
 					rel="stylesheet"
-					href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=optional"
+					href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=block"
 				/>
 			</Head>
 			<body>

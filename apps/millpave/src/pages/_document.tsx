5160b88d1d4dd7d2bff1298ccbb96142f5b94836
Justin Ikeako <justinikeako@gmail.com>
2022-12-11 14:37:15 -0500

    Make iconfont display optional to improve lighthouse score

--- apps/millpave/src/pages/_document.tsx
+++ apps/millpave/src/pages/_document.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/google-font-display */
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
@@ -7,7 +6,7 @@ export default function Document() {
 			<Head>
 				<link
 					rel="stylesheet"
-					href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=block"
+					href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=optional"
 				/>
 			</Head>
 			<body>

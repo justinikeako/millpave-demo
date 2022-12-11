80797d0586b146eb657fa25cfc4fcd9ec76a7fe4
Justin Ikeako <justinikeako@gmail.com>
2022-12-11 03:17:38 -0500

    Let me put it in a language you can understand 🫘

--- apps/millpave/src/sections/inspiration.tsx
+++ apps/millpave/src/sections/inspiration.tsx
@@ -1,18 +1,18 @@
-import {
-	motion,
-	useAnimationFrame,
-	useMotionValue,
-	useScroll,
-	useSpring,
-	useTransform,
-	useVelocity,
-	wrap
-} from 'framer-motion';
+import { motion, useScroll, useTransform } from 'framer-motion';
 import Link from 'next/link';
 import { useRef } from 'react';
 import { Button } from '../components/button';
 
 function InspirationSection() {
+	const ref = useRef<HTMLDivElement>(null);
+
+	const { scrollYProgress } = useScroll({
+		target: ref,
+		offset: ['start end', 'end start']
+	});
+	const x1 = useTransform(scrollYProgress, [0, 1], [150, -150]);
+	const x2 = useTransform(scrollYProgress, [0, 1], [-150, 150]);
+
 	return (
 		<section className="flex flex-col space-y-16">
 			<div className="flex flex-col items-center">
@@ -28,82 +28,37 @@ function InspirationSection() {
 				</Button>
 			</div>
 
-			<div className="-mx-8 space-y-4 md:-mx-24 md:space-y-6 lg:-mx-32 lg:space-y-8">
-				<Marquee baseVelocity={2}>
+			<div
+				className="-mx-8 space-y-4 overflow-hidden md:-mx-24 md:space-y-6 lg:-mx-32 lg:space-y-8"
+				ref={ref}
+			>
+				<motion.div
+					className="flex h-[40vmin] justify-start gap-4 md:gap-6 lg:gap-8"
+					style={{ x: x1 }}
+				>
 					<div className="flex aspect-square shrink-0 bg-gray-200" />
-				</Marquee>
-				<Marquee baseVelocity={-2}>
 					<div className="flex aspect-square shrink-0 bg-gray-200" />
-				</Marquee>
+					<div className="flex aspect-square shrink-0 bg-gray-200" />
+					<div className="flex aspect-square shrink-0 bg-gray-200" />
+					<div className="flex aspect-square shrink-0 bg-gray-200" />
+					<div className="flex aspect-square shrink-0 bg-gray-200" />
+				</motion.div>
+				<motion.div
+					className="flex h-[40vmin] justify-end gap-4 md:gap-6 lg:gap-8"
+					style={{ x: x2 }}
+				>
+					<div className="flex aspect-square shrink-0 bg-gray-200" />
+					<div className="flex aspect-square shrink-0 bg-gray-200" />
+					<div className="flex aspect-square shrink-0 bg-gray-200" />
+					<div className="flex aspect-square shrink-0 bg-gray-200" />
+					<div className="flex aspect-square shrink-0 bg-gray-200" />
+					<div className="flex aspect-square shrink-0 bg-gray-200" />
+					<div className="flex aspect-square shrink-0 bg-gray-200" />
+					<div className="flex aspect-square shrink-0 bg-gray-200" />
+					<div className="flex aspect-square shrink-0 bg-gray-200" />
+				</motion.div>
 			</div>
 		</section>
-	);
-}
-
-type MarqueeProps = React.PropsWithChildren<{
-	baseVelocity: number;
-}>;
-
-function Marquee({ children, baseVelocity = 100 }: MarqueeProps) {
-	const baseX = useMotionValue(0);
-	const { scrollY } = useScroll();
-	const scrollVelocity = useVelocity(scrollY);
-	const smoothVelocity = useSpring(scrollVelocity, {
-		damping: 50,
-		stiffness: 400
-	});
-	const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
-		clamp: false
-	});
-
-	/**
-	 * This is a magic wrapping for the length of the text - you
-	 * have to replace for wrapping that works for you or dynamically
-	 * calculate
-	 */
-	const x = useTransform(baseX, (v) => `${wrap(0, -44.5, v)}%`);
-
-	const directionFactor = useRef<number>(1);
-	useAnimationFrame((t, delta) => {
-		let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
-
-		/**
-		 * This is what changes the direction of the scroll once we
-		 * switch scrolling directions.
-		 */
-		if (velocityFactor.get() < 0) {
-			directionFactor.current = -1;
-		} else if (velocityFactor.get() > 0) {
-			directionFactor.current = 1;
-		}
-
-		moveBy += directionFactor.current * moveBy * velocityFactor.get();
-
-		baseX.set(baseX.get() + moveBy);
-	});
-
-	/**
-	 * The number of times to repeat the child text should be dynamically calculated
-	 * based on the size of the text and viewport. Likewise, the x motion value is
-	 * currently wrapped between -20 and -45% - this 25% is derived from the fact
-	 * we have four children (100% / 4). This would also want deriving from the
-	 * dynamically generated number of children.
-	 */
-	return (
-		<div className=" overflow-hidden">
-			<motion.div
-				className="flex h-[40vmin] gap-4 md:gap-6 lg:gap-8"
-				style={{ x }}
-			>
-				{children}
-				{children}
-				{children}
-				{children}
-				{children}
-				{children}
-				{children}
-			</motion.div>
-		</div>
 	);
 }
 

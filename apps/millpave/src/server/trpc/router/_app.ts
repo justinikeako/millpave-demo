40e3c3aa9771401d97c01248e494e1cb02abf6df
Justin Ikeako <justinikeako@gmail.com>
2022-12-13 18:17:22 -0500

    CI appeased?

--- apps/millpave/src/server/trpc/router/_app.ts
+++ apps/millpave/src/server/trpc/router/_app.ts
@@ -1,12 +1,10 @@
 import { router } from '../trpc';
 import { categoryRouter } from './category';
 import { productRouter } from './product';
-import { quoteRouter } from './quote';
 
 export const appRouter = router({
 	category: categoryRouter,
-	product: productRouter,
-	quote: quoteRouter
+	product: productRouter
 });
 
 // export type definition of API
--- apps/millpave/src/types/quote.ts
+++
@@ -1,8 +0,0 @@
-import z from 'zod';
-import { quoteInputItem, unitEnum } from '../validators/quote';
-
-export type Unit = z.infer<typeof unitEnum>;
-
-export type QuoteInputItem = z.infer<typeof quoteInputItem>;
-
-export type QuoteItemMetadata = unknown;

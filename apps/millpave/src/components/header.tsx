866058a2c91ec25648d03942661b0821123f0607
Justin Ikeako <justinikeako@gmail.com>
2022-12-14 16:22:56 -0500

    Upgrade header with animations

--- apps/millpave/src/components/header.tsx
+++ apps/millpave/src/components/header.tsx
@@ -1,63 +1,207 @@
 import Link from 'next/link';
 import { Button } from './button';
-import { MdMenu } from 'react-icons/md';
-import { motion } from 'framer-motion';
+import { MdClose, MdMenu } from 'react-icons/md';
+import { AnimatePresence, motion, Variants } from 'framer-motion';
+import { forwardRef, useState } from 'react';
+import * as Dialog from '@radix-ui/react-dialog';
 
-type NavLinkProps = React.PropsWithChildren<{ href: string }>;
+type NavLinkProps = {
+	href: string;
+} & React.HTMLProps<HTMLLIElement>;
 
-function NavLink({ href, children }: NavLinkProps) {
+const NavLink = forwardRef<HTMLLIElement, NavLinkProps>(function NavLink(
+	{ href, children, ...props },
+	ref
+) {
 	return (
-		<li>
+		<li ref={ref} {...props}>
 			<Link href={href}>{children}</Link>
 		</li>
 	);
-}
+});
+
+const content: Variants = {
+	hide: {
+		opacity: 0,
+		transition: {
+			type: 'spring',
+			mass: 1,
+			damping: 30,
+			stiffness: 300
+		}
+	},
+	show: {
+		opacity: 1,
+		transition: {
+			type: 'spring',
+			mass: 1,
+			damping: 30,
+			stiffness: 300
+		}
+	}
+};
+
+const list: Variants = {
+	hide: {
+		transition: {
+			staggerChildren: 0.05
+		}
+	},
+	show: {
+		transition: {
+			staggerChildren: 0.05
+		}
+	}
+};
+
+const item: Variants = {
+	hide: {
+		opacity: 0,
+		x: -25,
+		transition: {
+			type: 'spring',
+			mass: 1,
+			damping: 30,
+			stiffness: 300
+		}
+	},
+	show: {
+		opacity: 1,
+		x: 0,
+		transition: {
+			type: 'spring',
+			mass: 1,
+			damping: 30,
+			stiffness: 300
+		}
+	}
+};
+
+const variants = {
+	content,
+	list,
+	item
+};
+
+const MotionNavLink = motion(NavLink);
 
 function Header() {
+	const [open, setOpen] = useState(false);
+
 	return (
-		<motion.header
-			initial={{ y: 100, opacity: 0 }}
-			animate={{ y: 0, opacity: 1 }}
-			transition={{ type: 'spring', stiffness: 100, damping: 20 }}
-			className=" mb-12 select-none px-8 py-8 md:px-24 lg:px-32 lg:text-black"
-		>
-			<nav className="flex items-center justify-between">
-				<input type="checkbox" id="menu-toggle" className="peer hidden" />
+		<Dialog.Root open={open} modal onOpenChange={setOpen}>
+			<motion.header
+				initial={{ y: 100, opacity: 0 }}
+				animate={{ y: 0, opacity: 1 }}
+				transition={{ type: 'spring', stiffness: 100, damping: 20 }}
+				className="mb-12 select-none px-8 py-8 md:px-24 lg:px-32"
+			>
+				<nav className="flex items-center justify-between">
+					<Link href="/" className="text-lg font-bold">
+						Logo
+					</Link>
 
-				<Link
-					href="/"
-					className="z-20 text-lg font-bold peer-checked:text-white"
-				>
-					Logo
-				</Link>
+					{/* Desktop Links */}
+					<ul className="flex flex-row items-center gap-8 bg-transparent px-0 font-body text-base font-normal text-gray-900 max-lg:hidden">
+						<NavLink href="/products/all">Products</NavLink>
+						<NavLink href="/gallery">Get Inspired</NavLink>
+						<NavLink href="/#where-to-buy">Where to Buy</NavLink>
+						<NavLink href="/contact">Contact Us</NavLink>
+						<li>
+							<Button variant="primary" asChild>
+								<Link href="/contact?form=quote">Get A Quote</Link>
+							</Button>
+						</li>
+					</ul>
 
-				<ul className="inset-0 hidden flex-col items-start justify-center gap-8 bg-gray-900 px-16 text-3xl text-white  peer-checked:fixed peer-checked:flex max-lg:z-10 md:px-24 lg:static lg:flex lg:flex-row lg:items-center lg:bg-transparent lg:px-0 lg:font-body lg:text-base lg:font-normal lg:text-gray-900">
-					<NavLink href="/products/all">Products</NavLink>
-					<NavLink href="/gallery">Get Inspired</NavLink>
-					<NavLink href="/#where-to-buy">Where to Buy</NavLink>
-					<NavLink href="/contact">Contact Us</NavLink>
-					<li>
-						<Button
-							variant="primary"
-							asChild
-							className="max-lg:!p-0 max-lg:shadow-none"
-						>
-							<Link href="/contact?form=quote">Get A Quote</Link>
+					{/* Mobile Menu */}
+					<AnimatePresence>
+						{open && (
+							<Dialog.DialogPortal forceMount>
+								<Dialog.Overlay />
+								<Dialog.DialogContent forceMount asChild>
+									<motion.div
+										variants={variants.content}
+										initial="hide"
+										animate="show"
+										exit="hide"
+										className="fixed inset-0 flex flex-col bg-gray-900 text-white"
+									>
+										<div className="flex items-center justify-between px-8 py-8 md:px-24">
+											<Link
+												href="/"
+												className="text-lg font-bold"
+												onClick={() => setOpen(false)}
+											>
+												Logo
+											</Link>
+
+											<Dialog.Close asChild>
+												<Button
+													variant="tertiary"
+													className="text-white active:bg-gray-800"
+												>
+													<MdClose />
+												</Button>
+											</Dialog.Close>
+										</div>
+
+										<motion.ul
+											variants={variants.list}
+											initial="hide"
+											animate="show"
+											className="flex flex-1 flex-col items-start justify-center gap-8 px-16 text-3xl md:px-24"
+										>
+											<MotionNavLink
+												variants={variants.item}
+												href="/products/all"
+												onClick={() => setOpen(false)}
+											>
+												Products
+											</MotionNavLink>
+											<MotionNavLink
+												variants={variants.item}
+												href="/gallery"
+												onClick={() => setOpen(false)}
+											>
+												Get Inspired
+											</MotionNavLink>
+											<MotionNavLink
+												variants={variants.item}
+												href="/#where-to-buy"
+												onClick={() => setOpen(false)}
+											>
+												Where to Buy
+											</MotionNavLink>
+											<MotionNavLink
+												variants={variants.item}
+												href="/contact"
+												onClick={() => setOpen(false)}
+											>
+												Contact Us
+											</MotionNavLink>
+											<MotionNavLink
+												variants={variants.item}
+												href="/contact?form=quote"
+												onClick={() => setOpen(false)}
+											>
+												Get A Quote
+											</MotionNavLink>
+										</motion.ul>
+									</motion.div>
+								</Dialog.DialogContent>
+							</Dialog.DialogPortal>
+						)}
+					</AnimatePresence>
+
+					<Dialog.Trigger asChild>
+						<Button variant="tertiary" className=" lg:hidden">
+							<MdMenu />
 						</Button>
-					</li>
-				</ul>
-
-				<Button
-					variant="tertiary"
-					className="z-20 peer-checked:text-white peer-checked:active:bg-gray-800 lg:hidden"
-					asChild
-				>
-					<label htmlFor="menu-toggle">
-						<MdMenu />
-					</label>
-				</Button>
-			</nav>
-		</motion.header>
+					</Dialog.Trigger>
+				</nav>
+			</motion.header>
+		</Dialog.Root>
 	);
 }
 

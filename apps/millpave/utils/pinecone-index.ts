a93c6ff7cea859f7a25ecd6fd07ae169f6562e00
Justin Ikeako <justinikeako@gmail.com>
2023-04-15 13:52:28 -0500

    Make pinecone environment and index public

--- apps/millpave/utils/pinecone-index.ts
+++ apps/millpave/utils/pinecone-index.ts
@@ -5,10 +5,10 @@ const client = new PineconeClient();
 async function initPineconeIndex() {
 	await client.init({
 		apiKey: process.env.PINECONE_API_KEY as string,
-		environment: process.env.PINECONE_ENVIRONMENT as string
+		environment: process.env.NEXT_PUBLIC_PINECONE_ENVIRONMENT as string
 	});
 
-	return client.Index(process.env.PINECONE_INDEX as string);
+	return client.Index(process.env.NEXT_PUBLIC_PINECONE_INDEX as string);
 }
 
 export { initPineconeIndex };

ccc94d1606e81cabb01e7b67c769beb0a9da9ca9
Justin Ikeako <justinikeako@gmail.com>
2023-04-16 14:30:31 -0500

    Fixed type error on demo query

--- apps/millpave/src/scripts/demo-query.ts
+++ apps/millpave/src/scripts/demo-query.ts
@@ -1,8 +1,9 @@
-import { initPineconeIndex } from '../utils/pinecone-index';
+import { pinecone } from '../utils/pinecone-client';
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
 import { VectorDBQAChain } from 'langchain/chains';
 import { openai } from '../utils/openai-client';
+import { PINECONE_INDEX_NAME } from '../config/pinecone';
 
 const query =
 	'How much would 405 sqft of grasscrete and 34 sqft of colonial classic cost? Also, what are the dimensions?';
@@ -10,7 +11,7 @@ const query =
 const model = openai;
 
 async function searchForDocs() {
-	const pineconeIndex = await initPineconeIndex();
+	const pineconeIndex = pinecone.Index(PINECONE_INDEX_NAME);
 	const vectorStore = await PineconeStore.fromExistingIndex(
 		new OpenAIEmbeddings(),
 		{ pineconeIndex }

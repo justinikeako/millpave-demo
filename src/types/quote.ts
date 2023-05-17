743a9208fed419c6459119dc979044af0e94c8f1
Justin Ikeako <justinikeako@gmail.com>
2023-05-17 17:23:29 -0500

    Prepare zod validators for quote form

--- src/types/quote.ts
+++ src/types/quote.ts
@@ -1,34 +1,91 @@
-type Unit1D = 'ft' | 'in' | 'm' | 'cm';
-type Unit2D = 'sqft' | 'sqin' | 'sqm' | 'sqcm';
-export type Unit = Unit1D | Unit2D | 'fr' | 'pal' | 'unit';
+import { z } from 'zod';
+
+const Shape = z.enum(['rect', 'circle', 'arbitrary']);
+export type Shape = z.infer<typeof Shape>;
+
+const Unit1D = z.enum(['ft', 'in', 'm', 'cm']);
+type Unit1D = z.infer<typeof Unit1D>;
+
+const Unit2D = z.enum(['sqft', 'sqin', 'sqm', 'sqcm']);
+type Unit2D = z.infer<typeof Unit2D>;
+
+const CoverageUnit = z.enum(['fr', 'pal', 'unit']);
+
+export const Unit = z.union([Unit1D, Unit2D, CoverageUnit]);
+export type Unit = z.infer<typeof Unit>;
+
+const DimensionInput1D = z.object({
+	value: z.number(),
+	unit: Unit1D
+});
+type DimensionInput1D = z.infer<typeof DimensionInput1D>;
+
+const DimensionInput2D = z.object({
+	value: z.number(),
+	unit: Unit2D
+});
+type DimensionInput2D = z.infer<typeof DimensionInput2D>;
+
+const CoverageInput1D = z.object({
+	value: z.number(),
+	unit: z.union([Unit1D, CoverageUnit])
+});
+type CoverageInput1D = z.infer<typeof CoverageInput1D>;
+
+const CoverageInput2D = z.object({
+	value: z.number(),
+	unit: z.union([Unit2D, CoverageUnit])
+});
+type CoverageInput2D = z.infer<typeof CoverageInput1D>;
+
+const Stone1D = z.object({
+	skuId: z.string(),
+	displayName: z.string(),
+
+	coverage: CoverageInput1D
+});
+export type Stone1D = z.infer<typeof Stone1D>;
+
+const Stone2D = z.object({
+	skuId: z.string(),
+	displayName: z.string(),
+
+	coverage: CoverageInput2D
+});
+export type Stone2D = z.infer<typeof Stone2D>;
+
+const Stone = z.union([Stone1D, Stone2D]);
+export type Stone = z.infer<typeof Stone>;
 
 export type Coverage = { value: number; unit: Unit };
 
-export type Stone = {
-	skuId: string;
-	displayName: string;
+const Dimensions = z.object({
+	width: DimensionInput1D,
+	length: DimensionInput1D,
+	diameter: DimensionInput1D,
+	circumference: DimensionInput1D,
+	area: DimensionInput2D,
+	runningFoot: DimensionInput1D
+});
+export type Dimensions = z.infer<typeof Dimensions>;
 
-	coverage: Coverage;
-};
+export const Infill = Stone2D.array();
 
-export type Shape = 'rect' | 'circle' | 'arbitrary';
+export const Border = z.object({
+	runningFoot: DimensionInput1D,
+	orientation: z.enum(['SOLDIER_ROW', 'TIP_TO_TIP']),
+	stones: Stone1D.array()
+});
+export type Border = z.infer<typeof Border>;
 
-export type Dimensions = {
-	width: { value: number; unit: Unit1D };
-	length: { value: number; unit: Unit1D };
-	diameter: { value: number; unit: Unit1D };
-	circumference: { value: number; unit: Unit1D };
-	area: { value: number; unit: Unit2D };
-	runningFoot: { value: number; unit: Unit1D };
-};
+export const StoneProject = z.object({
+	shape: Shape,
 
-export type StoneProject = {
-	shape: Shape;
-	dimensions: Dimensions;
-	infill: Stone[];
-	border: {
-		runningFoot: { value: number; unit: Unit1D };
-		orientation: 'SOLDIER_ROW' | 'TIP_TO_TIP';
-		stones: Stone[];
-	};
-};
+	dimensions: Dimensions,
+
+	infill: Infill,
+
+	border: Border
+});
+
+export type StoneProject = z.infer<typeof StoneProject>;

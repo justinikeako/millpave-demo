fcbeca114c1b64c6bc37068c8be878259c73b47e
Justin Ikeako <justinikeako@gmail.com>
2023-05-22 01:51:35 -0500

    Remove unused icon from footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { Logo } from './logo';
-import { FacebookIcon, InstagramIcon, YoutubeIcon } from 'lucide-react';
+import { FacebookIcon, InstagramIcon } from 'lucide-react';
 
 function Footer() {
 	return (

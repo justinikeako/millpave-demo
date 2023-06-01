e543390454a90f43d026bc9fc83e7447b919a2a4
Justin Ikeako <justinikeako@gmail.com>
2023-05-31 22:43:08 -0500

    Fix drizzle config pointing to old schema and output paths

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-	schema: './drizzle/schema.ts',
-	out: './drizzle',
+	schema: './src/drizzle/schema.ts',
+	out: './src/drizzle',
 	connectionString: process.env.PROD_DATABASE_URL
 } satisfies Config;

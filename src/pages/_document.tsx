793cbfe1d1d77c6b1d40e28337f701dcc348cd99
Justin Ikeako <justinikeako@gmail.com>
2023-06-24 14:36:38 -0500

    Add appropriate attributes to sprite preload link

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
 	return (
 		<Html lang="en-US" className="text-gray-900">
 			<Head>
-				<link rel="preload" as="image/svg+xml" href="sprite.svg" />
+				<link rel="preload" as="image" type="image/svg+xml" href="sprite.svg" />
 			</Head>
 			<body className="bg-gray-100 text-gray-900">
 				<Main />

9f798abf6f45089c411ae8a087c3752841da93eb
Justin Ikeako <justinikeako@gmail.com>
2023-06-24 17:17:22 -0500

    Tweak the button style

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -13,14 +13,14 @@ type ButtonProps = {
 >;
 
 const buttonVariants = cva(
-	'flex items-center justify-center gap-1 font-semibold text-center select-none focus:outline-pink-700 focus:outline-offset-2 disabled:pointer-events-none disabled:shadow-none disabled:opacity-50 [&>span]:z-[1]',
+	'flex items-center justify-center gap-1 font-semibold text-center select-none focus:outline-pink-700 focus:outline-offset-2 disabled:shadow-none disabled:opacity-50 disabled:cursor-not-allowed [&>span]:z-[1] [&>svg]:z-[1]',
 	{
 		variants: {
 			intent: {
 				primary:
-					'relative px-3 py-2 rounded-sm overflow-hidden before:absolute before:block before:inset-px before:rounded-[3px] after:absolute after:block after:inset-px after:rounded-[3px] shadow-button',
+					'relative px-3 py-2 border rounded-sm overflow-hidden before:absolute before:block before:inset-0 before:rounded-[3px] after:absolute after:block after:inset-0 after:rounded-[3px] shadow-button',
 				secondary:
-					'rounded-sm border px-3 py-2 font-semibold transition-colors active:transition-none',
+					'rounded-sm px-3 py-2 border font-semibold transition-colors active:transition-none',
 				tertiary:
 					'-m-3 rounded-full p-3 transition-colors active:transition-none'
 			},
@@ -35,7 +35,7 @@ const buttonVariants = cva(
 				backdrop: 'light',
 				className: [
 					'bg-gradient-to-b bg-gray-900 text-gray-100',
-					'before:bg-gray-100/50 before:gradient-mask-b-0 before:opacity-50 before:transition-opacity hover:before:opacity-60 active:before:opacity-50 active:before:transition-none active:before:gradient-mask-t-0',
+					'border-gray-950 before:bg-gray-100/50 before:gradient-mask-b-0 before:opacity-50 before:transition-opacity hover:before:opacity-60 active:before:opacity-50 active:before:transition-none active:before:gradient-mask-t-0',
 					'after:border after:border-gray-100/50 after:gradient-mask-b-0 after:opacity-50 after:transition-opacity hover:after:opacity-60 active:after:opacity-50'
 				]
 			},
@@ -43,7 +43,7 @@ const buttonVariants = cva(
 				intent: 'primary',
 				backdrop: 'dark',
 				className: [
-					'bg-gradient-to-b bg-gray-300 text-gray-600',
+					'bg-gradient-to-b bg-gray-300 text-gray-600 border-gray-400',
 					'before:bg-white/90 before:gradient-mask-b-0 before:opacity-75 before:transition-opacity hover:before:opacity-100 active:before:opacity-50 active:before:transition-none active:before:gradient-mask-t-0',
 					'after:border after:border-white/50 after:gradient-mask-b-0 after:opacity-75 after:transition-opacity hover:after:opacity-100 active:after:opacity-60'
 				]

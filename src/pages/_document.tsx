5aa5f3928a359a571f207877e37942eb5c2aa393
Justin Ikeako <justinikeako@gmail.com>
2023-06-27 07:41:29 -0500

    Get rid of preload warnings

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,9 +4,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
 	return (
 		<Html lang="en-US" className="text-gray-900">
-			<Head>
-				<link rel="preload" as="image" type="image/svg+xml" href="sprite.svg" />
-			</Head>
+			<Head />
 			<body className="bg-gray-100 text-gray-900">
 				<Main />
 				<NextScript />

6c3d70a8472896949a098da0fd1c3645e19c4eb5
Justin Ikeako <justinikeako@gmail.com>
2023-06-27 18:15:35 -0500

    Disable trpc ssr boolean (should re-enable Next's static optimization)

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -56,8 +56,9 @@ export const api = createTRPCNext<AppRouter>({
 			]
 		};
 	},
-	ssr: true
+	ssr: false
 });
+
 const getBaseUrl = () => {
 	if (typeof window !== 'undefined') return ''; // browser should use relative url
 	if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url

04da6e7b21194de65fe465a4dfc4bea1676c63ce
Justin Ikeako <justinikeako@gmail.com>
2023-06-29 00:35:51 -0500

    Remove group styles from button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -36,8 +36,8 @@ const buttonVariants = cva(
 				className: [
 					// Main styles
 					'bg-gray-900 text-gray-100 border-gray-950 before:bg-gray-100/50 before:opacity-50 after:border-gray-100/50 after:gradient-mask-b-0 after:opacity-50',
-					// Focused
-					'hover:before:opacity-60 hover:after:opacity-60 group-hover:before:opacity-60 group-hover:after:opacity-60 group-focus-within:before:opacity-60 group-focus-within:after:opacity-60',
+					// Hover
+					'hover:before:opacity-60 hover:after:opacity-60',
 					// Active
 					'active:before:gradient-mask-t-0 active:before:opacity-50 active:after:opacity-50'
 				]
@@ -48,8 +48,8 @@ const buttonVariants = cva(
 				className: [
 					// Main styles
 					'bg-gray-300 text-gray-600 border-gray-400 before:bg-white/90 before:opacity-75 after:border after:border-white/50 after:gradient-mask-b-0 after:opacity-75',
-					// Focused
-					'hover:before:opacity-100 hover:after:opacity-100 group-hover:before:opacity-100 group-hover:after:opacity-100 group-focus-within:before:opacity-100 group-focus-within:after:opacity-100',
+					// Hover
+					'hover:before:opacity-100 hover:after:opacity-100',
 					// Active
 					'active:before:gradient-mask-t-0 active:before:opacity-50 active:after:opacity-60'
 				]
@@ -58,13 +58,13 @@ const buttonVariants = cva(
 				intent: 'secondary',
 				backdrop: 'light',
 				className:
-					'border-gray-900/25 hover:bg-gray-900/5 group-hover:bg-gray-900/5 group-focus-within:bg-gray-900/5 active:!bg-gray-900/10'
+					'border-gray-900/25 hover:bg-gray-900/5 active:bg-gray-900/10'
 			},
 			{
 				intent: 'secondary',
 				backdrop: 'dark',
 				className:
-					'text-gray-100 border-gray-100/25 hover:bg-gray-100/5 group-hover:bg-gray-100/5 group-focus-within:bg-gray-100/5 active:!bg-gray-100/10'
+					'text-gray-100 border-gray-100/25 hover:bg-gray-100/5 active:bg-gray-100/10'
 			},
 			{
 				intent: 'tertiary',

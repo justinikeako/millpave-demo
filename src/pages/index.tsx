a3959952d84a0746768052d9fec72dc464c83059
Justin Ikeako <justinikeako@gmail.com>
2023-07-16 22:05:16 -0500

    Turn homepage hero section into a slideshow

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,14 +3,14 @@ import {
 	motion,
 	MotionStyle,
 	Transition,
-	useCycle,
+	useInView,
 	useScroll,
 	useTransform
 } from 'framer-motion';
 import Head from 'next/head';
 import { Footer } from '~/components/footer';
 import Link from 'next/link';
-import { useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Balancer } from 'react-wrap-balancer';
 import { Button } from '~/components/button';
 import { ProductCard } from '~/components/product-card';
@@ -25,16 +25,34 @@ import { HorizontalScroller } from '~/components/horizontal-scroller';
 import Image from 'next/image';
 import { GetAQuoteSection } from '~/components/sections/get-a-quote';
 
-function Hero() {
-	const heroRef = useRef<HTMLDivElement>(null);
-	const [example, cycleExample] = useCycle(
-		'Driveway',
-		'Patio',
-		'Plaza',
-		'Garden',
-		'Pool Deck'
-	);
+function useCycle<T>(array: T[]) {
+	const [currentIndex, setCurrentIndex] = useState(0);
 
+	const currentElement = array[currentIndex] as T;
+	const nextIndex = currentIndex === array.length - 1 ? 0 : currentIndex + 1;
+	const nextElement = array[nextIndex] as T;
+
+	const cycle = () => {
+		setCurrentIndex(nextIndex);
+	};
+
+	return [currentElement, nextElement, cycle] as const;
+}
+
+const MotionImage = motion(Image);
+function Hero() {
+	const useCases: { imageSrc: string; text: string }[] = [
+		{ text: 'Driveway', imageSrc: '/driveway.jpg' },
+		{ text: 'Patio', imageSrc: '/patio.jpg' },
+		{ text: 'Plaza', imageSrc: '/plaza.jpg' },
+		{ text: 'Garden', imageSrc: '/garden.jpg' },
+		{ text: 'Pool Deck', imageSrc: '/pool-deck.jpg' }
+	];
+	const cycleDuration = 7.5; // seconds
+
+	const [imageLoadPriority, setImageLoadPriority] = useState(true);
+	const heroRef = useRef<HTMLDivElement>(null);
+	const [current, next, cycleUseCase] = useCycle(useCases);
 	const { scrollY, scrollYProgress } = useScroll({
 		target: heroRef,
 		offset: ['end end', 'end start']
@@ -43,17 +61,32 @@ function Hero() {
 		target: heroRef,
 		offset: ['start start', 'end start']
 	});
+
+	const line1Ref = useRef<HTMLDivElement>(null);
+	const line1InView = useInView(line1Ref);
 	const line1Opacity = useTransform(scrollY, [50, 150], [1, 0]);
 	const line2Opacity = useTransform(scrollY, [50, 200], [1, 0]);
 
 	useEffect(() => {
-		// Set an interval to cycle through examples state variable after 5 seconds
-		const intervalId = setInterval(() => {
-			cycleExample();
-		}, 5000);
+		const timeoutId = setTimeout(() => {
+			setImageLoadPriority(true);
+		}, cycleDuration * 1000);
+
+		return () => clearTimeout(timeoutId);
+	}, [imageLoadPriority, cycleDuration]);
+
+	useEffect(() => {
+		let intervalId: NodeJS.Timer;
+
+		// Set an interval to cycle through the steps after 7.5 seconds
+		if (line1InView) {
+			intervalId = setInterval(() => {
+				cycleUseCase();
+			}, cycleDuration * 1000);
+		}
 
 		return () => clearInterval(intervalId);
-	}, [cycleExample]);
+	}, [cycleDuration, line1InView, cycleUseCase]);
 
 	const fastTransition: Transition = {
 		type: 'spring',
@@ -85,18 +118,32 @@ function Hero() {
 				<motion.div
 					initial={{ opacity: 0 }}
 					animate={{ opacity: 1 }}
-					transition={{ duration: 0.25 }}
-					className="-z-50 h-full w-full bg-gray-900"
+					exit={{ opacity: 0 }}
+					transition={{ duration: 1 }}
+					className="relative -z-50 h-full w-full scale-[calc(1+var(--scroll-progress-from-start)*0.25)] bg-gray-900"
 				>
 					<Image
-						src="/hero.jpg"
-						priority
-						fetchPriority="high"
-						width={1000}
-						height={747}
+						key={next.imageSrc}
+						fill
+						src={next.imageSrc}
 						alt="A large driveway paved with neutral-tone concrete pavers."
-						className=" h-full w-full scale-[calc(1+var(--scroll-progress-from-start)*0.25)] bg-gray-900 object-cover object-center"
+						className="absolute left-0 top-0 h-full w-full bg-gray-900 object-cover object-center opacity-0"
 					/>
+					<AnimatePresence>
+						<MotionImage
+							key={current.imageSrc}
+							fill
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							exit={{ opacity: 0, zIndex: 1 }}
+							transition={{ duration: 1 }}
+							src={current.imageSrc}
+							priority={imageLoadPriority}
+							fetchPriority={imageLoadPriority ? 'high' : 'auto'}
+							alt="A large driveway paved with neutral-tone concrete pavers."
+							className="absolute left-0 top-0 h-full w-full bg-gray-900 object-cover object-center"
+						/>
+					</AnimatePresence>
 				</motion.div>
 			</motion.div>
 
@@ -107,7 +154,9 @@ function Hero() {
 						style={{ opacity: line1Opacity }}
 					>
 						<OrchestratedReveal asChild delay={0.1}>
-							<span className="inline-block">Transform Your</span>
+							<span ref={line1Ref} className="inline-block">
+								Transform Your
+							</span>
 						</OrchestratedReveal>
 					</motion.span>
 					<br />
@@ -119,18 +168,18 @@ function Hero() {
 							<span className="inline-block">
 								<AnimatePresence initial={false} mode="wait">
 									<motion.span
-										key={example}
+										key={current.text}
 										className="inline-block"
 										exit={{ y: -10, opacity: 0, transition: fastTransition }}
 									>
-										{(example + '.').split('').map((char, index) => {
+										{(current.text + '.').split('').map((char, index) => {
 											if (char === ' ')
 												return <span key={char + index}>&nbsp;</span>;
 
 											return (
 												<motion.span
 													key={char + index}
-													initial={{ y: 10, opacity: 0 }}
+													initial={{ y: 25, opacity: 0 }}
 													transition={{
 														delay: index / 10,
 														...slowTransition

8db2c53ccd0bcef2ef9c50eb2c835942b36665f1
Justin Ikeako <justinikeako@gmail.com>
2023-07-17 01:50:22 -0500

    Add images to learn section

--- src/components/sections/learn.tsx
+++ src/components/sections/learn.tsx
@@ -3,12 +3,21 @@ import Link from 'next/link';
 import { Icon } from '../icon';
 import { Balancer } from 'react-wrap-balancer';
 import { ViewportReveal } from '../reveal';
+import Image from 'next/image';
 
 function LearnSection() {
 	return (
 		<ViewportReveal asChild>
 			<section className="grid grid-cols-1 gap-4 py-16 sm:grid-cols-2 xl:grid-cols-4">
-				<div className="group relative flex h-96 shrink-0 flex-col items-start gap-2 bg-gray-600 p-6 text-white sm:col-span-2 sm:justify-between">
+				<div className="group relative flex h-[28rem] shrink-0 flex-col items-start gap-2 bg-gray-600 p-6 text-white sm:col-span-2 sm:justify-between">
+					<div className="absolute bottom-0 right-0 h-full w-3/5 md:w-1/2">
+						<Image
+							src="/contractor.png"
+							alt="Paving stones around a firepit"
+							fill
+							className="pointer-events-none object-contain object-bottom"
+						/>
+					</div>
 					<div className="max-w-xs space-y-2">
 						<Link
 							href="/contractors"
@@ -41,7 +50,13 @@ function LearnSection() {
 					</Button>
 				</div>
 
-				<div className="group relative flex h-96 shrink-0 flex-col items-start justify-end space-y-2 bg-gray-200 p-6">
+				<div className="group relative flex h-[28rem] shrink-0 flex-col items-start justify-end space-y-2 bg-gray-200 p-6">
+					<Image
+						src="/resources.png"
+						alt="Paving stones around a firepit"
+						fill
+						className="pointer-events-none object-contain object-top"
+					/>
 					<div className="max-w-xs space-y-1">
 						<Link href="/resources" className="before:absolute before:inset-0">
 							<h3 className="font-display text-lg">Resources</h3>
@@ -63,7 +78,13 @@ function LearnSection() {
 					</Button>
 				</div>
 
-				<div className="group relative flex h-96 shrink-0 flex-col items-start justify-start space-y-2 bg-gray-200 p-6">
+				<div className="group relative flex h-[28rem] shrink-0 flex-col items-start justify-start space-y-2 bg-gray-200 p-6">
+					<Image
+						src="/blog.png"
+						alt="Paving stones around a firepit"
+						fill
+						className="pointer-events-none object-contain object-bottom"
+					/>
 					<div className="max-w-xs space-y-1">
 						<Link
 							href="/blog/why-use-pavers"

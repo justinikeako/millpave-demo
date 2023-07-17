6a1ac947bac813436dcfeaaec670f74eab23e287
Justin Ikeako <justinikeako@gmail.com>
2023-07-17 01:50:44 -0500

    Add image to AR gallery section

--- src/components/sections/ar-gallery.tsx
+++ src/components/sections/ar-gallery.tsx
@@ -2,11 +2,22 @@ import { SplitSection } from './split';
 import { Button } from '~/components/button';
 import { Icon } from '~/components/icon';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export function AugmentedRealityGallerySection() {
 	return (
 		<SplitSection
-			slot={<div className="aspect-square bg-gray-300 py-0 md:aspect-auto" />}
+			slot={
+				<div className="relative aspect-square py-0 md:aspect-auto">
+					<Image
+						src="/ar.png"
+						alt="An iPhone featuring a patio design in augmented reality"
+						fill
+						sizes="(max-width: 480px) 90vw, (max-width: 768px) 50vw, (max-width: 1536px) 33vw"
+						className="object-contain"
+					/>
+				</div>
+			}
 			tagline="AR Sample Gallery"
 			heading="See our pavers in your space."
 			body="Use your smartphone camera to visualize and virtually place paving stones in real-time, right in your own space."

95ddcb82b475676f0ae9d9e9765405d1d74b3568
Justin Ikeako <justinikeako@gmail.com>
2023-10-07 18:35:09 -0500

    Remove unnecessary important modifier from checkbox

--- src/components/checkbox.tsx
+++ src/components/checkbox.tsx
@@ -25,7 +25,7 @@ export const Checkbox = forwardRef<React.ElementRef<'input'>, CheckboxProps>(
 
 				<div
 					className={cn(
-						'group relative h-5 w-5 rounded-sm border border-gray-400 bg-gray-100 from-white/50 text-gray-100 outline-2 outline-gray-900 hover:border-gray-500 active:border-gray-600 peer-checked:!border-pink-700 peer-checked:bg-pink-600 peer-checked:bg-gradient-to-b peer-checked:hover:from-white/60 peer-checked:active:bg-gradient-to-t peer-checked:active:from-white/25 peer-focus-visible:outline',
+						'group relative h-5 w-5 rounded-sm border border-gray-400 bg-gray-100 from-white/50 text-gray-100 outline-2 outline-gray-900 hover:border-gray-500 active:border-gray-600 peer-checked:border-pink-700 peer-checked:bg-pink-600 peer-checked:bg-gradient-to-b peer-checked:hover:from-white/60 peer-checked:active:bg-gradient-to-t peer-checked:active:from-white/25 peer-focus-visible:outline',
 						className
 					)}
 				>

efe2a834c254164336bd5af0794fb0b4c0f6ba5a
Justin Ikeako <justinikeako@gmail.com>
2024-05-30 17:12:58 -0500

    Presumably the last merge conflict?

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import type { Config } from 'drizzle-kit';
-import 'dotenv/config';
-
-export default {
-	schema: './src/drizzle/schema.ts',
-	out: './src/drizzle',
-	driver: 'mysql2',
-	dbCredentials: {
-		connectionString: process.env.PROD_DATABASE_URL as string
-	},
-=======
 import { type Config } from 'drizzle-kit';
 import { env } from '~/env';
 
@@ -17,6 +5,5 @@ export default {
 	schema: './src/server/db/schema.ts',
 	driver: 'pg',
 	dbCredentials: { connectionString: env.POSTGRES_URL },
->>>>>>> app-dir
 	tablesFilter: ['millpave_*']
 } satisfies Config;
